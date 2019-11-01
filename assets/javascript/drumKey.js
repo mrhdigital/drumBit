8c50d15962b0fba760e5ea1623fb4a2dc9a6c482
unknown <mrhdigital@yahoo.com>
2019-10-31 22:18:56 -0500

    audio is not playing

--- assets/javascript/drumKey.js
+++ assets/javascript/drumKey.js
@@ -1,5 +1,9 @@
 // alert("js is linked");
 
 window.addEventListener("keydown", function(e) {
-    console.log(e);
+    //console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
+    console.log(audio);
+    //audio.play();
+    //audio.play();
 });
