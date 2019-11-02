83d7205173b4701990336469bb64127ebd070c11
unknown <mrhdigital@yahoo.com>
2019-11-01 20:50:26 -0500

    key A sound is working

--- assets/javascript/drumKey.js
+++ assets/javascript/drumKey.js
@@ -3,8 +3,10 @@
 window.addEventListener("keydown", function(e) {
     //console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
-    console.log(audio);
-    
+    console.log(audio);    
+   
+    if(!audio) 
+    return;
     audio.play();
     
 });
